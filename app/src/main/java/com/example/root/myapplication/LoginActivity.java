4c8e9161629d8451448d0b8e1203bbd4ed899aa5
Muhannad511 <Skaro@live.com>
2016-02-11 19:14:05 -0800

    Deleted the variable

--- app/src/main/java/com/example/root/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/root/myapplication/LoginActivity.java
@@ -1,5 +1,5 @@
 package com.example.root.myapplication;
-
+   //I delete the variable
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
@@ -44,7 +44,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * Id to identity READ_CONTACTS permission request.
      */
     private static final int REQUEST_READ_CONTACTS = 0;
-   private static final int muhannadVar = 5;
+
+
     /**
      * A dummy authentication store containing known user names and passwords.
      * TODO: remove after connecting to a real authentication system.
